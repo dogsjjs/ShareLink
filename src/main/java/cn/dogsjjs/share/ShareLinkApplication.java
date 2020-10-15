2c2bca1a72e35750e4fe285f356b96315dcb2770
dogsjjs <1428204486@qq.com>
2020-10-15 22:53:48 +0800

    框架搭建

---
+++ src/main/java/cn/dogsjjs/share/ShareLinkApplication.java
@@ -0,0 +1,13 @@
+package cn.dogsjjs.share;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareLinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShareLinkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/dogsjjs/share/ShareLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.dogsjjs.share;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareLinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
